867ccc109b6df95f42a721cc638fcec851fd543a
Raghad <raghadbassam387@gmail.com>
2026-03-26 14:28:36 +0300

    update file puplic and privit request

--- apiTest.py
+++ apiTest.py
@@ -1,12 +1,104 @@
-from fastapi import FastAPI 
-app = FastAPI()
+from fastapi import FastAPI, Request, HTTPException
+from dotenv import load_dotenv
+import os
 
+app = FastAPI()
+load_dotenv()
 items = []
 
+API_KEY = os.getenv("API_KEY")
+
+def check_api_key(request: Request):
+    api_key = request.headers.get("x-api-key")
+    if api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    
+
+@app.get("/objects")
+def get_items(request: Request):
+    check_api_key(request)
+    return items
+
+@app.post("/objects")
+def add_item(item: dict, request: Request):
+    check_api_key(request)
+    item["id"] = len(items) + 1
+    items.append(item)
+    return {"message": "added"}
+
+@app.get("/objects/{item_id}")
+def get_objectsbyID(item_ID:int, request: Request):
+    check_api_key(request)
+    for item in items:
+        if item[id]==item_ID:
+         return item
+    return {"error":"not found"}
+
+@app.put("/objects/{item_id}")
+def update_object(item_ID:int, item_update:dict,request:Request):
+       check_api_key(request)
+       for item in items:
+          if item[id]==item_ID:
+             items[items.index(item)]=item_update
+             return {"message":"update is done"}
+          return {"error":"not found"}
+   
+@app.patch("/objects/{item_id}")
+def update_object(item_ID:int, item_update:dict ,request:Request):
+   check_api_key(request)
+   for item in items:
+      if item["id"]==item_ID:
+        item.update(item_update)
+        return item 
+      return {"error":"not found"}
+
+@app.delete("/objects/{item_id}")
+def update_object(item_id:int, request:Request):
+   check_api_key(request)
+   for item in items:
+      if item[id]==item_id:
+         items.remove(item)
+         return {"message":"remove is done"}
+      return {"error":"not found"} 
+   
 @app.get("/objects")
 def get_obgects():
     return items
+
+@app.get("/objects/{item_id}")
+def get_objectsbyID(item_ID:int):
+    for item in items:
+        if item[id]==item_ID:
+         return item
+    return {"error":"not found"}
+
 @app.post("/objects")
 def add_objects(item:dict):
+    item["id"] =len(items)+1
     items.append(item)
     return{"message":"added"}
+
+@app.put("/objects/{item_id}")
+def update_object(item_ID:int , item_updated:dict):
+   for item in items:
+      if item[id]==item_ID:
+        items[items.index(item)]=item_updated
+        return {"message":"update is done"}
+   return {"error":"not fount"}
+
+@app.patch("/objects/{item_id}")
+def update_object(item_ID:int, item_update:dict):
+   for item in items:
+      if item[id]==item_ID:
+       item.update(item_update)
+       return items
+   return {"message":"not found"}
+         
+@app.delete("/objects/{item_id}")
+def delete_object(item_ID:int, item_delete:dict):
+   for item in items:
+      if item[id]==item_ID:
+         items.remove(item)
+         return {"message":"deleted is done"}
+      return {"error":"not found"}
+
