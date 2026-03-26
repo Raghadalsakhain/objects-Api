9444f9a32d74bc357aa35cefe20820f191b7a563
Raghad <raghadbassam387@gmail.com>
2026-03-26 14:34:05 +0300

    add to privet depends

--- apiTest.py
+++ apiTest.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException, Depends
 from dotenv import load_dotenv
 import os
 
@@ -6,25 +6,17 @@ app = FastAPI()
 load_dotenv()
 items = []
 
-API_KEY = os.getenv("API_KEY")
+API_KEY  = os.getenv("API_KEY")
 
 def check_api_key(request: Request):
     api_key = request.headers.get("x-api-key")
     if api_key != API_KEY:
         raise HTTPException(status_code=401, detail="Unauthorized")
     
-
+##privet 
 @app.get("/objects")
-def get_items(request: Request):
-    check_api_key(request)
+def get_items(dep=Depends(check_api_key)):
     return items
-
-@app.post("/objects")
-def add_item(item: dict, request: Request):
-    check_api_key(request)
-    item["id"] = len(items) + 1
-    items.append(item)
-    return {"message": "added"}
 
 @app.get("/objects/{item_id}")
 def get_objectsbyID(item_ID:int, request: Request):
@@ -34,6 +26,14 @@ def get_objectsbyID(item_ID:int, request: Request):
          return item
     return {"error":"not found"}
 
+@app.post("/objects")
+def add_item(item: dict, request: Request):
+    check_api_key(request)
+    item["id"] = len(items) + 1
+    items.append(item)
+    return {"message": "added"}
+
+
 @app.put("/objects/{item_id}")
 def update_object(item_ID:int, item_update:dict,request:Request):
        check_api_key(request)
@@ -42,6 +42,7 @@ def update_object(item_ID:int, item_update:dict,request:Request):
              items[items.index(item)]=item_update
              return {"message":"update is done"}
           return {"error":"not found"}
+       
    
 @app.patch("/objects/{item_id}")
 def update_object(item_ID:int, item_update:dict ,request:Request):
@@ -52,6 +53,7 @@ def update_object(item_ID:int, item_update:dict ,request:Request):
         return item 
       return {"error":"not found"}
 
+
 @app.delete("/objects/{item_id}")
 def update_object(item_id:int, request:Request):
    check_api_key(request)
@@ -61,6 +63,9 @@ def update_object(item_id:int, request:Request):
          return {"message":"remove is done"}
       return {"error":"not found"} 
    
+
+   ##public 
+
 @app.get("/objects")
 def get_obgects():
     return items
@@ -72,33 +77,36 @@ def get_objectsbyID(item_ID:int):
          return item
     return {"error":"not found"}
 
-@app.post("/objects")
-def add_objects(item:dict):
-    item["id"] =len(items)+1
-    items.append(item)
-    return{"message":"added"}
+# @app.post("/objects")
+# def add_objects(item:dict):
+#     item["id"] =len(items)+1
+#     items.append(item)
+#     return{"message":"added"}
 
-@app.put("/objects/{item_id}")
-def update_object(item_ID:int , item_updated:dict):
-   for item in items:
-      if item[id]==item_ID:
-        items[items.index(item)]=item_updated
-        return {"message":"update is done"}
-   return {"error":"not fount"}
 
-@app.patch("/objects/{item_id}")
-def update_object(item_ID:int, item_update:dict):
-   for item in items:
-      if item[id]==item_ID:
-       item.update(item_update)
-       return items
-   return {"message":"not found"}
-         
-@app.delete("/objects/{item_id}")
-def delete_object(item_ID:int, item_delete:dict):
-   for item in items:
-      if item[id]==item_ID:
-         items.remove(item)
-         return {"message":"deleted is done"}
-      return {"error":"not found"}
+# @app.put("/objects/{item_id}")
+# def update_object(item_ID:int , item_updated:dict):
+#    for item in items:
+#       if item[id]==item_ID:
+#         items[items.index(item)]=item_updated
+#         return {"message":"update is done"}
+#    return {"error":"not fount"}
+
+
+# @app.patch("/objects/{item_id}")
+# def update_object(item_ID:int, item_update:dict):
+#    for item in items:
+#       if item[id]==item_ID:
+#        item.update(item_update)
+#        return items
+#    return {"message":"not found"}
+
+
+# @app.delete("/objects/{item_id}")
+# def delete_object(item_ID:int, item_delete:dict):
+#    for item in items:
+#       if item[id]==item_ID:
+#          items.remove(item)
+#          return {"message":"deleted is done"}
+#       return {"error":"not found"}
 
